c3853007e02142274cf8328f71db43dabf206f05
Vítor Norberto <vithor.norberto@gmail.com>
2021-05-03 19:33:53 -0300

    Corrige emissão de relatorio

--- src/main/java/com/rafa/web/api/service/RelatorioService.java
+++ src/main/java/com/rafa/web/api/service/RelatorioService.java
@@ -1,17 +1,19 @@
 package com.rafa.web.api.service;
 
 import net.sf.jasperreports.engine.*;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 
 @Service
 public class RelatorioService {
 
-    public byte[] gerarRelatorio() throws FileNotFoundException, JRException {
-        String caminhoRelatorio = ResourceUtils.getFile("classpath:static/relatorios/ficha.jrxml").getAbsolutePath();
+    public byte[] gerarRelatorio() throws IOException, JRException {
+        // String caminhoRelatorio = ResourceUtils.getFile("classpath:static/relatorios/ficha.jrxml").getAbsolutePath();
+        InputStream caminhoRelatorio = new ClassPathResource("static/relatorios/ficha.jrxml").getInputStream();
         JasperReport jasperReport = JasperCompileManager.compileReport(caminhoRelatorio);
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap<>(), new JREmptyDataSource());
         return JasperExportManager.exportReportToPdf(jasperPrint);
