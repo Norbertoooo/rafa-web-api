9f5ba260fe191523bbb264a8f73aa63eacd8221b
Vítor Norberto <vithor.norberto@gmail.com>
2021-05-30 22:15:35 -0300

    Adiciona banner e log ao inicializar aplicação

--- src/main/java/com/rafa/web/api/RafaWebApiApplication.java
+++ src/main/java/com/rafa/web/api/RafaWebApiApplication.java
@@ -1,13 +1,29 @@
 package com.rafa.web.api;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Log4j2
 public class RafaWebApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RafaWebApiApplication.class, args);
+        Environment env = SpringApplication.run(RafaWebApiApplication.class, args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                env.getActiveProfiles()
+        );
     }
 
 }
