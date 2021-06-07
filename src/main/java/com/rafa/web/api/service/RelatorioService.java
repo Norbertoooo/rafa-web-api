735863c9a32861a1abd3a08d2f16d7a18e17856d
Vítor Norberto <vithor.norberto@gmail.com>
2021-06-07 17:36:41 -0300

    Adiciona relatorio com informações do paciente, terapeuta e responsaveis

--- src/main/java/com/rafa/web/api/service/RelatorioService.java
+++ src/main/java/com/rafa/web/api/service/RelatorioService.java
@@ -1,21 +1,45 @@
 package com.rafa.web.api.service;
 
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.util.JRSaver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class RelatorioService {
 
-    public byte[] gerarRelatorio() throws IOException, JRException {
+    @Autowired
+    private DataSource dataSource;
+
+    public byte[] gerarRelatorio(Integer pacienteId) throws IOException, JRException, SQLException {
         // String caminhoRelatorio = ResourceUtils.getFile("classpath:static/relatorios/ficha.jrxml").getAbsolutePath();
+
         InputStream caminhoRelatorio = new ClassPathResource("static/relatorios/ficha.jrxml").getInputStream();
+        InputStream caminhoRelatorioResponsavel = new ClassPathResource("static/relatorios/responsaveis.jrxml").getInputStream();
+        InputStream caminhoRelatorioTerapeuta = new ClassPathResource("static/relatorios/terapeuta.jrxml").getInputStream();
+
         JasperReport jasperReport = JasperCompileManager.compileReport(caminhoRelatorio);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap<>(), new JREmptyDataSource());
+        JasperReport jasperReportResponsavel = JasperCompileManager.compileReport(caminhoRelatorioResponsavel);
+        JasperReport jasperReportTerapeuta = JasperCompileManager.compileReport(caminhoRelatorioTerapeuta);
+
+        JRSaver.saveObject(jasperReportTerapeuta, "terapeuta.jasper");
+        JRSaver.saveObject(jasperReportResponsavel, "responsaveis.jasper");
+
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("pacienteId", pacienteId);
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource.getConnection());
+
         return JasperExportManager.exportReportToPdf(jasperPrint);
     }
 }
--- src/main/java/com/rafa/web/api/web/RelatorioResource.java
+++ src/main/java/com/rafa/web/api/web/RelatorioResource.java
@@ -4,12 +4,14 @@ import com.rafa.web.api.service.RelatorioService;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/api/relatorios")
@@ -22,14 +24,14 @@ public class RelatorioResource {
         this.relatorioService = relatorioService;
     }
 
-    @GetMapping
-    public void exportarRelatorioFichaPaciente(HttpServletResponse response) throws IOException, JRException {
+    @GetMapping("/{pacienteId}")
+    public void exportarRelatorioFichaPaciente(HttpServletResponse response, @PathVariable Integer pacienteId) throws IOException, JRException, SQLException {
         // Configura a resposta para o tipo PDF
         response.setContentType("application/pdf");
         // Define que o arquivo pode ser visualizado no navegador e também nome final do arquivo
         // para fazer download do relatório troque 'inline' por 'attachment'
         response.setHeader("Content-Disposition", "inline; filename=ficha.pdf");
         OutputStream outStream = response.getOutputStream();
-        outStream.write(relatorioService.gerarRelatorio());
+        outStream.write(relatorioService.gerarRelatorio(pacienteId));
     }
 }
