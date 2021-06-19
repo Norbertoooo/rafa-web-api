ddbb0e157d60b1bab0193d4ed4006d182a410dc4
Vítor Norberto <vithor.norberto@gmail.com>
2021-06-19 00:07:44 -0300

    Adiciona configuração para testes

--- src/test/java/com/rafa/web/api/web/ResponsavelResourceTest.java
+++ src/test/java/com/rafa/web/api/web/ResponsavelResourceTest.java
@@ -1,12 +1,20 @@
 package com.rafa.web.api.web;
 
+import com.rafa.web.api.config.jwt.JwtAuthenticationEntryPoint;
+import com.rafa.web.api.config.jwt.JwtTokenUtil;
+import com.rafa.web.api.config.jwt.JwtUserDetailsService;
 import com.rafa.web.api.service.ResponsavelService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -16,10 +24,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(ResponsavelResource.class)
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 public class ResponsavelResourceTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @MockBean
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @MockBean
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @MockBean
+    private JwtTokenUtil jwtTokenUtil;
 
     @MockBean
     private ResponsavelService responsavelServiceMock;
@@ -28,7 +53,7 @@ public class ResponsavelResourceTest {
     public void listarResponsaveis() throws Exception {
         when(responsavelServiceMock.listarResponsaveis(0, 10)).thenReturn(Page.empty());
         this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/responsaveis"))
+                .get("/api/responsaveis"))
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful());
     }
--- src/test/java/com/rafa/web/api/web/TerapeutaResourceTest.java
+++ src/test/java/com/rafa/web/api/web/TerapeutaResourceTest.java
@@ -1,14 +1,22 @@
 package com.rafa.web.api.web;
 
+import com.rafa.web.api.config.jwt.JwtAuthenticationEntryPoint;
+import com.rafa.web.api.config.jwt.JwtTokenUtil;
+import com.rafa.web.api.config.jwt.JwtUserDetailsService;
 import com.rafa.web.api.domain.Terapeuta;
 import com.rafa.web.api.service.TerapeutaService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -17,12 +25,29 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(TerapeutaResource.class)
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class TerapeutaResourceTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @MockBean
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @MockBean
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @MockBean
+    private JwtTokenUtil jwtTokenUtil;
 
     @MockBean
     private TerapeutaService terapeutaServiceMock;
@@ -32,7 +57,7 @@ public class TerapeutaResourceTest {
         Terapeuta terapeuta = montarTerapeutaMock();
         when(terapeutaServiceMock.cadastrarTerapeuta(terapeuta)).thenReturn(terapeuta);
         this.mockMvc.perform(MockMvcRequestBuilders
-                .post("/terapeutas")
+                .post("/api/terapeutas")
                 .content(converterParaJson(terapeuta))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -43,7 +68,7 @@ public class TerapeutaResourceTest {
     public void listarTerapeutas() throws Exception {
         when(terapeutaServiceMock.listarTerapeutas(0, 10)).thenReturn(Page.empty());
         this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/terapeutas"))
+                .get("/api/terapeutas"))
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful());
     }
@@ -52,7 +77,7 @@ public class TerapeutaResourceTest {
     public void buscarTerapeutaPeloId() throws Exception {
         when(terapeutaServiceMock.buscarTerapeutaPeloId(1L)).thenReturn(Terapeuta.builder().construir());
         this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/terapeutas").param("id", "1"))
+                .get("/api/terapeutas").param("id", "1"))
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful());
     }
